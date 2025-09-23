8ff55bf3d04d971e119a5780e5cd543de3131670
szczukabendeguz <szczukabendeguz@gmail.com>
2025-09-24 00:13:01 +0200

    Creates Question DTO
    Defines a Data Transfer Object (DTO) for representing question data.
    
    This DTO encapsulates properties such as question text, options,
    hardness level, and associated programming language information,
    facilitating data transfer between different layers of the application.

---
+++ CodeLingo.API/DTOs/QuestionDto.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeLingo.API.DTOs
+{
+    public class QuestionDto
+    {
+        public int Id { get; set; }
+        public int ProgrammingLanguageId { get; set; }
+        public string ProgrammingLanguageName { get; set; } = null!;
+        public string QuestionText { get; set; } = null!;
+        public string Option1 { get; set; } = null!;
+        public string Option2 { get; set; } = null!;
+        public string Option3 { get; set; } = null!;
+        public string Option4 { get; set; } = null!;
+        public int HardnessLevel { get; set; }  // 1=Easy,2=Medium,3=Hard
+        public string HardnessLevelText { get; set; } = null!;
+    }
+}
