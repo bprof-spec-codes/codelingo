cfd426c76637031559be4f7e6368d813770d6b08
szczukabendeguz <szczukabendeguz@gmail.com>
2025-09-24 00:16:05 +0200

    Initializes the Angular frontend application
    Sets up the basic structure of an Angular application, including:
    
    - Core modules and components
    - Routing configuration
    - Basic HTML layout with a navigation bar
    - Styles for the application

---
+++ CodeLingo.Frontend/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DemoComponent } from './demo/demo.component';
+import { provideHttpClient } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DemoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideHttpClient()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
