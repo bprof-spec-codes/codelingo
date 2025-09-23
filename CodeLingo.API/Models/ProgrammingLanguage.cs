1b5a5a2853d679a40ce923fa2f80da5fd4af97aa
szczukabendeguz <szczukabendeguz@gmail.com>
2025-09-24 00:13:35 +0200

    Adds data models for questions and languages
    Defines the data models for programming languages and questions.
    
    These models establish the structure for storing and retrieving question data.

---
+++ CodeLingo.API/Models/ProgrammingLanguage.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeLingo.API.Models
+{
+    public class ProgrammingLanguage
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string ShortCode { get; set; } = null!;
+        public ICollection<Question> Questions { get; set; } = new List<Question>();
+    }
+}
---
+++ CodeLingo.API/Models/Question.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeLingo.API.Models
+
+{
+    public enum HardnessLevel { Easy = 1, Medium = 2, Hard = 3 }
+    public class Question
+    {
+        public int Id { get; set; }
+        public int ProgrammingLanguageId { get; set; }
+        public ProgrammingLanguage? ProgrammingLanguage { get; set; }
+        public string QuestionText { get; set; } = null!;
+        public string Option1 { get; set; } = null!;
+        public string Option2 { get; set; } = null!;
+        public string Option3 { get; set; } = null!;
+        public string Option4 { get; set; } = null!;
+        public int CorrectOptionNumber { get; set; }  // 1-4
+        public HardnessLevel HardnessLevel { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
