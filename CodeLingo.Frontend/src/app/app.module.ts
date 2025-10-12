80286dbaef695300204df2827e93c35890e4cb51
koboribence <kobori.bence@stud.uni-obuda.hu>
2025-10-12 10:13:18 +0200

    navbar created

--- CodeLingo.Frontend/src/app/app.module.ts
+++ CodeLingo.Frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
 import { provideHttpClient } from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent
+    DemoComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
