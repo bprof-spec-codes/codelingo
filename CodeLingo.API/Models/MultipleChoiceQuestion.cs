84ee9adc091071b3f07cca9317a21f29c82c8396
szczukabendeguz <szc.bende@stud.uni-obuda.hu>
2025-10-20 22:37:43 +0200

    add MultipleChoiceQuestion entity with options

---
+++ CodeLingo.API/Models/MultipleChoiceQuestion.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CodeLingo.API.Models
+{
+    public class MultipleChoiceQuestion
+    {
+        [Key]
+        [ForeignKey("Question")]
+        public string QuestionId { get; set; }
+
+        [Required]
+        [Column(TypeName = "json")]
+        public string Options { get; set; } // JSON array of AnswerOption
+
+        [Required]
+        [Column(TypeName = "json")]
+        public string CorrectAnswerIds { get; set; } // JSON array of strings
+
+        public bool AllowMultipleSelection { get; set; } = false;
+        public bool ShuffleOptions { get; set; } = true;
+
+        // Navigation property
+        public virtual Question Question { get; set; }
+    }
+}
+}
