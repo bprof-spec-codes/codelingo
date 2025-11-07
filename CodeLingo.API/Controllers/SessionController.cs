237f9a69201041f1c4b442e8c1a91676acaf2552
Misih26 <homonnai001@stud.uni-obuda.hu>
2025-11-07 20:07:49 +0100

    Refactor SessionController: replace GET with POST for Create using StartSessionRequestDto, add Update, Delete, Read, and ReadAll endpoints

--- CodeLingo.API/Controllers/SessionController.cs
+++ CodeLingo.API/Controllers/SessionController.cs
@@ -1,6 +1,7 @@
 ﻿using CodeLingo.API.Logics;
 using CodeLingo.API.Models;
 using Microsoft.AspNetCore.Mvc;
+using static CodeLingo.API.DTOs.Session.SessionDtos;
 
 namespace CodeLingo.API.Controllers
 {
@@ -13,10 +14,33 @@ namespace CodeLingo.API.Controllers
         {
             this.sessionLogic = sessionLogic;
         }
-        [HttpGet]
-        public void Create([FromBody] Session session)
+        [HttpPost("create")]
+        public void Create([FromBody] StartSessionRequestDto session)
         {
             sessionLogic.Create(session);
         }
+
+        [HttpPut("update")]
+        public void Update([FromBody] Session session) 
+        { 
+            this.sessionLogic.Update(session);
+        }
+
+        [HttpDelete("delete")]
+        public void Delete([FromBody] Session session) 
+        { 
+            this.sessionLogic.Delete(session);
+        }
+        [HttpGet("read")]
+        public Session Read(string id)
+        {
+            return this.sessionLogic.Read(id);
+        }
+        [HttpGet("read-all")]
+        public List<Session> ReadAll()
+        {
+           return this.sessionLogic.ReadAll();
+        }
+
     }
 }
