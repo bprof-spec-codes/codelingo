a822c2fe5814151186a84a48872f83ebfa100d42
Misih26 <homonnai001@stud.uni-obuda.hu>
2025-11-07 18:24:03 +0100

    Created SessionLogic

---
+++ CodeLingo.API/Logics/SessionLogic.cs
@@ -0,0 +1,20 @@
+﻿using CodeLingo.API.Models;
+using CodeLingo.API.Repositories;
+
+namespace CodeLingo.API.Logics
+{
+    public class SessionLogic
+    {
+        private ISessionRepository repository;
+        public SessionLogic(ISessionRepository repository)
+        {
+                this.repository = repository;
+        }
+        
+        public void Create(Session session)
+        {
+            repository.Create(session);
+        }
+       
+    }
+}
