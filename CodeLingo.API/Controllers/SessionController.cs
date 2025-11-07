d36e1f9743fa79e2dbcbe20f488c7e84d522457a
Misih26 <homonnai001@stud.uni-obuda.hu>
2025-11-07 18:24:56 +0100

    Created SessionController

---
+++ CodeLingo.API/Controllers/SessionController.cs
@@ -0,0 +1,22 @@
+﻿using CodeLingo.API.Logics;
+using CodeLingo.API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodeLingo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SessionController : ControllerBase
+    {
+        private SessionLogic sessionLogic;
+        public SessionController(SessionLogic sessionLogic)
+        {
+            this.sessionLogic = sessionLogic;
+        }
+        [HttpGet]
+        public void Create([FromBody] Session session)
+        {
+            sessionLogic.Create(session);
+        }
+    }
+}
