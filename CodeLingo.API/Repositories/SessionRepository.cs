2243db33fd4dcee40c2508678b03696be45be1f0
Misih26 <homonnai001@stud.uni-obuda.hu>
2025-11-07 12:58:51 +0100

    Session repository implementation added

---
+++ CodeLingo.API/Repositories/SessionRepository.cs
@@ -0,0 +1,19 @@
+﻿using CodeLingo.API.Data;
+using CodeLingo.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeLingo.API.Repositories
+{
+    public class SessionRepository : ISessionRepository
+    {
+        private AppDbContext DbContext;
+        public SessionRepository(AppDbContext dbContext)
+        {
+                this.DbContext = dbContext;
+        }
+        public void Create(Session entity)
+        {
+            this.DbContext.Add(entity);
+        }
+    }
+}
