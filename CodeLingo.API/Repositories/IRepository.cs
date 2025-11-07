08b551ba6315ec23d7b77cfab375572c177f1b28
Misih26 <homonnai001@stud.uni-obuda.hu>
2025-11-07 20:10:38 +0100

    Refactor IRepository<T>: add Delete, Read, ReadAll methods and SaveChanges to support full CRUD

--- CodeLingo.API/Repositories/IRepository.cs
+++ CodeLingo.API/Repositories/IRepository.cs
@@ -3,5 +3,10 @@
     public interface IRepository<T>
     {
         public void Create(T entity);
+        public void SaveChanges();
+        public void Delete(T entity);
+        public T Read(string id);
+        public List<T> ReadAll();
+
     }
 }
