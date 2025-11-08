e90139252a6b550a9ab6d58dc3a9a846fdf2f153
Misih26 <homonnai001@stud.uni-obuda.hu>
2025-11-08 12:08:42 +0100

    feat(session): extend session creation to include random questions and return response DTO

--- CodeLingo.API/Logics/SessionLogic.cs
+++ CodeLingo.API/Logics/SessionLogic.cs
@@ -8,19 +8,43 @@ namespace CodeLingo.API.Logics
     public class SessionLogic
     {
         private ISessionRepository repository;
-        public SessionLogic(ISessionRepository repository)
+        private QuestionRepository questionRepository;
+        private SessionQuestionRepository sessionQuestionRepository;
+        public SessionLogic(ISessionRepository repository, QuestionRepository questionRepository, SessionQuestionRepository sessionQuestionRepository)
         {
                 this.repository = repository;
+                this.questionRepository = questionRepository;
+                this.sessionQuestionRepository = sessionQuestionRepository;
         }
-        
-        public void Create(StartSessionRequestDto session)
+
+        public StartSessionResponseDto Create(StartSessionRequestDto session)
         {
             Session DatabaseSession = new Session();
             DatabaseSession.UserId = session.UserId;
             DatabaseSession.Language = session.Language;
             DatabaseSession.Difficulty = (DifficultyLevel)Enum.Parse(typeof(DifficultyLevel),session.Difficulty);
             DatabaseSession.DesiredCount = session.RequestedQuestionCount;
+
             repository.Create(DatabaseSession);
+
+            List<Question> questions = this.questionRepository.getRandomQuestions(session.RequestedQuestionCount);
+            List<SessionQuestion> sessionQuestions = new List<SessionQuestion>();
+            foreach (Question question in questions)
+            {
+                SessionQuestion sessionQuestion = new SessionQuestion();
+                sessionQuestion.SessionId = DatabaseSession.Id;
+                sessionQuestion.QuestionId = question.Id;
+                sessionQuestionRepository.Create(sessionQuestion);
+                sessionQuestions.Add(sessionQuestion);
+            }
+
+            DatabaseSession.SessionQuestions = sessionQuestions;
+            repository.SaveChanges();
+
+            StartSessionResponseDto startSessionResponseDto = new StartSessionResponseDto();
+            startSessionResponseDto.SessionId = DatabaseSession.Id;
+            startSessionResponseDto.TotalPlannedQuestions = DatabaseSession.DesiredCount;
+            return startSessionResponseDto;
         }
         public void Update(Session session)
         {
