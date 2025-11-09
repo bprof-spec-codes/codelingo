6cc9872082c44bf4c9dbe852fffc3690609b3a0c
Prohaszka Adam <prohaszka.adam00@gmail.com>
2025-11-09 16:41:10 +0100

    updated header buttons to follow the user state

--- CodeLingo.Frontend/src/app/header/header.component.ts
+++ CodeLingo.Frontend/src/app/header/header.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth/auth.service';
 
 @Component({
   selector: 'app-navigation-header',
   standalone: false,
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
+  isLoggedIn$;
 
+  constructor(private auth: AuthService) {
+    this.isLoggedIn$ = this.auth.isLoggedIn$;
+  }
+
+  logout() {
+    this.auth.logout();
+  }
 }
