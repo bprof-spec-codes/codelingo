c01ce5f0f4d4186d86ddf59e736bf447862bf69b
oli-tolnai <oli.tolnai3@gmail.com>
2025-11-09 16:38:41 +0100

    Add multiple choice question component

---
+++ CodeLingo.Frontend/src/app/question-container/multiple-choice-question/multiple-choice-question.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multiple-choice-question',
+  standalone: false,
+  templateUrl: './multiple-choice-question.component.html',
+  styleUrl: './multiple-choice-question.component.scss'
+})
+export class MultipleChoiceQuestionComponent {
+
+}
