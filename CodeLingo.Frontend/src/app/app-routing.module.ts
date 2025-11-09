4380dc5200be1aa5666a677710639a2cac09bdcf
Prohaszka Adam <prohaszka.adam00@gmail.com>
2025-11-09 16:39:29 +0100

    added login route

--- CodeLingo.Frontend/src/app/app-routing.module.ts
+++ CodeLingo.Frontend/src/app/app-routing.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { PracticeStarterComponent } from './practice-starter/practice-starter.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'landing-page', pathMatch: 'full'},
-  {path: 'landing-page', component: LandingPageComponent},
-  {path: 'practice/start', component: PracticeStarterComponent},
-  {path: '**', redirectTo: 'landing-page', pathMatch: 'full'}
+  { path: '', redirectTo: 'landing-page', pathMatch: 'full' },
+  { path: 'landing-page', component: LandingPageComponent },
+  { path: 'practice/start', component: PracticeStarterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: 'landing-page', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
