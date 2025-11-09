1472b5fdbdfe058ca7578822bdf678949e4aadc4
Prohaszka Adam <prohaszka.adam00@gmail.com>
2025-11-09 16:39:47 +0100

    added modules

--- CodeLingo.Frontend/src/app/app.module.ts
+++ CodeLingo.Frontend/src/app/app.module.ts
@@ -3,7 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  HTTP_INTERCEPTORS,
+  HttpClientModule,
+  provideHttpClient,
+} from '@angular/common/http';
 import { MainLayoutComponent } from './main-layout/main-layout.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
@@ -11,9 +15,12 @@ import { FooterComponent } from './footer/footer.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { PracticeStarterComponent } from './practice-starter/practice-starter.component';
 import { LanguageSelectorComponent } from './practice-starter/language-selector/language-selector.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { DifficultySelectorComponent } from './practice-starter/difficulty-selector/difficulty-selector.component';
 import { QuestionCountSelectorComponent } from './practice-starter/question-count-selector/question-count-selector.component';
+import { LoginComponent } from './login/login.component';
+import { AuthInterceptor } from './interceptors/auth.interceptor';
+import { AuthMockInterceptor } from './interceptors/auth-mock.interceptor';
 
 @NgModule({
   declarations: [
@@ -29,16 +36,21 @@ import { QuestionCountSelectorComponent } from './practice-starter/question-coun
     PracticeStarterComponent,
     DifficultySelectorComponent,
     PracticeStarterComponent,
-    QuestionCountSelectorComponent
+    QuestionCountSelectorComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   providers: [
-    provideHttpClient()
+    provideHttpClient(),
+    { provide: HTTP_INTERCEPTORS, useClass: AuthMockInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
