a674d214f69bbdaf3450a3f5c6c778a15fce9047
Misih26 <homonnai001@stud.uni-obuda.hu>
2025-11-11 22:06:25 +0100

    Add logging to SessionController and inject ILogger

--- CodeLingo.API/Controllers/SessionController.cs
+++ CodeLingo.API/Controllers/SessionController.cs
@@ -9,15 +9,21 @@ namespace CodeLingo.API.Controllers
     [ApiController]
     public class SessionController : ControllerBase
     {
+        private readonly ILogger<SessionController> _logger;
         private SessionLogic sessionLogic;
-        public SessionController(SessionLogic sessionLogic)
+        public SessionController(SessionLogic sessionLogic, ILogger<SessionController> logger)
         {
             this.sessionLogic = sessionLogic;
+            _logger = logger;
         }
         [HttpPost("start")]
         public StartSessionResponseDto Create([FromBody] StartSessionRequestDto session)
         {
-            return sessionLogic.Create(session);
+            // TODO: Authentication fill user ID
+            _logger.LogInformation("Session creation started");
+            StartSessionResponseDto response = sessionLogic.Create(session);
+            _logger.LogInformation("Session created");
+            return response;
         }
 
         [HttpPut("update")]
