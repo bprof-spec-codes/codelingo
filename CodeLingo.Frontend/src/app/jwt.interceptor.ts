15ed060b6a8b9a1462f09c7c0cce25b7106985a2
krisz1321 <erdeikrisz1321@gmail.com>
2025-11-14 22:09:20 +0100

    implement jwt interceptor to add authorization token for the requests

--- CodeLingo.Frontend/src/app/jwt.interceptor.ts
+++ CodeLingo.Frontend/src/app/jwt.interceptor.ts
@@ -1,5 +1,17 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  let request = req
+ 	 if (req.method === "GET" || req.method === "PUT") {	
+    const token = localStorage.getItem(environment.tokenKey)
+    if (token) {
+      request = req.clone({
+        setHeaders: { Authorization: `Bearer ${token}` }
+      })
+    }
+   }
+
+   return next(request);
 };
+
