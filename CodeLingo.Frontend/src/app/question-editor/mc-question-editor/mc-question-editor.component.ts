ed88589b71622a1bb79dc7df5f5052d921fcf022
oli-tolnai <oli.tolnai3@gmail.com>
2025-11-20 18:04:09 +0100

    create initializeForm() function

--- CodeLingo.Frontend/src/app/question-editor/mc-question-editor/mc-question-editor.component.ts
+++ CodeLingo.Frontend/src/app/question-editor/mc-question-editor/mc-question-editor.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MultipleChoiceQuestion } from '../../models/multiple-choice-question';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-mc-question-editor',
@@ -12,10 +13,30 @@ export class McQuestionEditorComponent {
   @Input() existingQuestion?: MultipleChoiceQuestion;
   @Input() languages: string[] = [];
   @Input() difficulties: string[] = [];
-  
+
   @Output() questionSubmitted = new EventEmitter<MultipleChoiceQuestion>();
   @Output() cancelled = new EventEmitter<void>();
 
-  
+  questionForm!: FormGroup;
+  questionId: string = '';
+  version: number = 1;
+
+  constructor(private fb: FormBuilder) { }
+
+  initializeForm(): void {
+    this.questionForm = this.fb.group({
+      title: ['', Validators.required],
+      language: ['', Validators.required],
+      difficulty: ['', Validators.required],
+      questionText: ['', Validators.required],
+      explanation: ['', Validators.required],
+      tagsInput: [''],
+      estimatedTimeSeconds: [30, [Validators.required, Validators.min(5), Validators.max(600)]],
+      allowMultipleSelection: [false],
+      shuffleOptions: [true],
+      isActive: [true],
+      options: this.fb.array([], Validators.minLength(2))
+    });
+  }
 
 }
