010d7e1d2a8d011d8304635ce5b3d7deb71babec
koboribence <kobori.bence@stud.uni-obuda.hu>
2025-11-20 14:40:20 +0100

    lanquages fix

--- CodeLingo.Frontend/src/app/admin/admin-list/admin-list.component.ts
+++ CodeLingo.Frontend/src/app/admin/admin-list/admin-list.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Question, QuestionType } from '../../models/question';
 import { AdminService } from '../../services/admin.service';
+import { Language } from '../../models/language';
 
 @Component({
   selector: 'app-admin-list',
@@ -13,6 +14,7 @@ export class AdminListComponent {
   @Input() questions$!: Observable<Question[]>;
   @Output() questionUpdated = new EventEmitter<Question>();
   @Output() delete = new EventEmitter<string>();
+  @Input() languages$!: Observable<Language[]>;
   onItemDelete(id: string) {
     this.delete.emit(id);
   }
@@ -22,7 +24,11 @@ export class AdminListComponent {
   QuestionType = QuestionType
   constructor(private service: AdminService) { }
 
-  onCreateQuestion() {
+  onEdit(lang: Language) {
+    // form megnyitása, modal, route, stb.
+  }
 
+  onDelete(lang: Language) {
+    // confirm + DELETE /admin/languages/{id}
   }
 }
