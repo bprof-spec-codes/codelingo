8cf80854adfd75507b164ed6ed4ea5d7d0bfcb48
oli-tolnai <oli.tolnai3@gmail.com>
2025-11-20 17:37:31 +0100

    Add input/output properties to question editor

--- CodeLingo.Frontend/src/app/question-editor/mc-question-editor/mc-question-editor.component.ts
+++ CodeLingo.Frontend/src/app/question-editor/mc-question-editor/mc-question-editor.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MultipleChoiceQuestion } from '../../models/multiple-choice-question';
 
 @Component({
   selector: 'app-mc-question-editor',
@@ -7,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './mc-question-editor.component.scss'
 })
 export class McQuestionEditorComponent {
+
+  @Input() existingQuestion?: MultipleChoiceQuestion;
+  @Input() languages: string[] = [];
+  @Input() difficulties: string[] = [];
+  
+  @Output() questionSubmitted = new EventEmitter<MultipleChoiceQuestion>();
+  @Output() cancelled = new EventEmitter<void>();
+
+  
 
 }
