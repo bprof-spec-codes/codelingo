e456f7942dc7a229debeaddb6dfeba02f1981303
Prohaszka Adam <prohaszka.adam00@gmail.com>
2025-11-22 23:08:45 +0100

    updated routing

--- CodeLingo.Frontend/src/app/app-routing.module.ts
+++ CodeLingo.Frontend/src/app/app-routing.module.ts
@@ -3,18 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 import { PracticeStarterComponent } from './practice-starter/practice-starter.component';
-
+import { QuestionContainerComponent } from './question-container/question-container.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'landing-page', pathMatch: 'full'},
-  {path: 'landing-page', component: LandingPageComponent},
-  {path: 'leaderboard', component: LeaderboardComponent},
-  {path: 'practice/start', component: PracticeStarterComponent},
-  {path: '**', redirectTo: 'landing-page', pathMatch: 'full'}
+  { path: '', redirectTo: 'landing-page', pathMatch: 'full' },
+  { path: 'landing-page', component: LandingPageComponent },
+  { path: 'leaderboard', component: LeaderboardComponent },
+  { path: 'practice/start', component: PracticeStarterComponent },
+  { path: 'session/:id/questions', component: QuestionContainerComponent },
+  { path: '**', redirectTo: 'landing-page', pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
