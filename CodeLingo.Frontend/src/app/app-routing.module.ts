43793ff2940cf6cea13285ad7daf805b0cd362d5
koboribence <kobori.bence@stud.uni-obuda.hu>
2025-11-23 10:25:40 +0100

    adminGuard added

--- CodeLingo.Frontend/src/app/app-routing.module.ts
+++ CodeLingo.Frontend/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
+import { adminGuard } from './guards/admin.guard';
 
 const routes: Routes = [
   {path: '', redirectTo: 'landing-page', pathMatch: 'full'},
   {path: 'landing-page', component: LandingPageComponent},
-    { path: 'admin', component: AdminPanelComponent },
+    { path: 'admin', component: AdminPanelComponent, /*canActivate:[adminGuard]*/},
   {path: '**', redirectTo: 'landing-page', pathMatch: 'full'}
 
 ];
