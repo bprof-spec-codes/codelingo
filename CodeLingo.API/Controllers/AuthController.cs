79cbfe317732d0eaa9e03ccec0b104102a022dca
Máté Zsebeházi <zsebehazimate@gmail.com>
2025-11-23 13:53:02 +0100

    AuthController init

--- CodeLingo.API/Controllers/AuthController.cs
+++ CodeLingo.API/Controllers/AuthController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CodeLingo.API.Logics;
+using CodeLingo.API.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CodeLingo.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/auth")]
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly AuthLogic _authLogic;
+        private readonly UserManager<User> _userManager;
+
+        public AuthController(AuthLogic authLogic, UserManager<User> userManager)
+        {
+            _authLogic = authLogic;
+            _userManager = userManager;
+        }
     }
 }
