34941cae72849743a0b3459708d0f24fd2bb2bf6
Máté Zsebeházi <zsebehazimate@gmail.com>
2025-12-03 13:50:52 +0100

    Created ProgressRepository

---
+++ CodeLingo.API/Repositories/ProgressRepository.cs
@@ -0,0 +1,52 @@
+﻿using CodeLingo.API.Data;
+using CodeLingo.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeLingo.API.Repositories
+{
+    public class ProgressRepository : IProgressRepository
+    {
+        private readonly AppDbContext _ctx;
+
+        public ProgressRepository(AppDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public void Create(Progress entity)
+        {
+            _ctx.Progresses.Add(entity);
+        }
+
+        public void Delete(Progress entity)
+        {
+            _ctx.Progresses.Remove(entity);
+        }
+
+        public Progress Read(string id)
+        {
+            return _ctx.Progresses
+                .Include(p => p.User)
+                .FirstOrDefault(p => p.UserId == id);
+        }
+
+        public Progress ReadByUserId(string userId)
+        {
+            return _ctx.Progresses
+                .Include(p => p.User)
+                .FirstOrDefault(p => p.UserId == userId);
+        }
+
+        public List<Progress> ReadAll()
+        {
+            return _ctx.Progresses
+                .Include(p => p.User)
+                .ToList();
+        }
+
+        public void SaveChanges()
+        {
+            _ctx.SaveChanges();
+        }
+    }
+}
