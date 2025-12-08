fafea086b9ccde5d2301e795a6e8b50b9008220d
Máté Zsebeházi <zsebehazimate@gmail.com>
2025-12-08 17:19:58 +0100

    Enhance result summary layout and styling

--- CodeLingo.Frontend/src/app/question-container/result-summary/result-summary.component.ts
+++ CodeLingo.Frontend/src/app/question-container/result-summary/result-summary.component.ts
@@ -9,4 +9,18 @@ import { SessionSummary } from '../../models/session-summary';
 })
 export class ResultSummaryComponent {
   @Input() sessionSummary!: SessionSummary
+
+  answeredPercentage = 0;
+  correctPercentage = 0;
+
+  ngOnInit() {
+    // Animate progress bars with a delay
+    setTimeout(() => {
+      this.answeredPercentage = (this.sessionSummary.answeredQuestions / this.sessionSummary.totalQuestions) * 100;
+      
+      setTimeout(() => {
+        this.correctPercentage = (this.sessionSummary.correctAnswers / this.sessionSummary.answeredQuestions) * 100;
+      }, 500);
+    }, 300);
+  }
 }
